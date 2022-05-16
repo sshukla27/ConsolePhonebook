ce8cf7731e33e62e7ebe0c1431f3dec3b036ef6b
shirishkolekar <61945126+shirishkolekar@users.noreply.github.com>
2022-05-16 21:30:28 +0530

    Minor Corrections done

--- com.adi/src/com/adi/Manager.java
+++ com.adi/src/com/adi/Manager.java
@@ -4,155 +4,110 @@ import java.util.Scanner;
 
 public class Manager {
 
-	public static int password=0000;
-	public static int userPw;
-	public static int numberInput; 
-	public static String searchinput;
-	
-	//Scanner sc=new Scanner(System.in);
-	static Contact[] con = new Contact[5];
-	
-	con[0]=con1;
-	con[1]=con2;		
-	con[2]=con3;
-	con[3]=con4;
-	con[4]=con5;
-	
-	public static void addContact()
-	{
-		int i;
-		
-		Scanner sc=new Scanner(System.in);
-		if(addmoreContact=='y' || addmoreContact=='Y' && i<5) 
-		{
-			for( i=0;i<5;i++)
-			{
-				System.out.print("\n"+"Enter Mobile number : ");
-				con[i].mobileNo= sc.nextInt();
-				
+	static int password = 0000;
+	static int userPw;
+	static String searchinput;
+
+	public static void addContact(Contact[] con, Scanner sc) {
+		char addmoreContact = 'y';
+		if (addmoreContact == 'y' || addmoreContact == 'Y') {
+			// fix this - at a time we will be adding single contact and not all at once,
+			// update this
+			for (int i = 0; i < 5; i++) {
+				System.out.print("\n" + "Enter Mobile number : ");
+				con[i].mobileNo = sc.nextInt();
+
 				System.out.print("Contact Name : ");
-				con[i].name= sc.next();
+				con[i].name = sc.next();
 
 				System.out.print("Email : ");
-				con[i].emailId= sc.next();
+				con[i].emailId = sc.next();
 
 				System.out.println("Contact Added");
 			}
-			System.out.print("\n"+"Do you want to add more contacts?.. y/n :");
-			addmoreContact=sc.next().charAt(0);
+			System.out.print("\n" + "Do you want to add more contacts?.. y/n :");
+			addmoreContact = sc.next().charAt(0);
 		}
 	}
-			
-	public static void searchContact()
-	{
-	
 
-		Scanner cc=new Scanner(System.in);
-		System.out.println("Search Contact based on Name or Email: ");		
-		String searchInput=cc.next();
-	
-		for(int i=0;i<5;i++)
-		{
-			if(name==searchInput || (emailId)==searchInput)
-			{
-			System.out.println(mobileNo+"\t"+name+"\t\t"+emailId);
+	public static void searchContact(Contact[] con, Scanner sc) {
+		System.out.println("Search Contact based on Name or Email: ");
+		String searchInput = sc.next();
+
+		for (int i = 0; i < 5; i++) {
+			// fix this - what is name emailId?
+			if (name == searchInput || (emailId) == searchInput) {
+				// fix this - way to access fields in class is incorrect
+				System.out.println(mobileNo + "\t" + name + "\t\t" + emailId);
 				break;
 			}
 		}
-		System.out.println(searchInput+" isn't available.");
+		System.out.println(searchInput + " isn't available.");
 	}
-		
-	
-	
-	public static void searchContactBymobileNo()
-	{
-		Scanner ac =new Scanner(System.in);
-		System.out.println("Search Contact based on mobile number: ");		
-		numberInput=ac.nextInt();
 
-		
-		for(int i=0;i<5;i++)
-		{
-			if(mobileNo==numberInput)
-			{
-				System.out.println(mobileNo+"\t"+name+"\t\t"+emailId);
+	public static void searchContactBymobileNo(Contact[] con, Scanner sc) {
+		int numberInput;
+		Scanner ac = new Scanner(System.in);
+		System.out.println("Search Contact based on mobile number: ");
+		numberInput = ac.nextInt();
+
+		for (int i = 0; i < 5; i++) {
+			// fix this - what is name mobileNo?
+			if (mobileNo == numberInput) {
+				// fix this - way to access fields in class is incorrect
+				System.out.println(mobileNo + "\t" + name + "\t\t" + emailId);
 				break;
 			}
 		}
-		System.out.println(numberInput+" isn't available.");
+		System.out.println(numberInput + " isn't available.");
 	}
-	
-	
-	
-	
-	
-	
-	
 
-	public static void showAllContacts()
-	{
-		System.out.println("Contact No."+"\t"+"Name"+"\t\t"+"Email Id");
-
-	
-		for(int i=0;i<5;i++)
-	{
-		
-			System.out.println(mobileNo+"\t"+name+"\t\t"+emailId);
+	public static void showAllContacts(Contact[] con) {
+		System.out.println("Contact No." + "\t" + "Name" + "\t\t" + "Email Id");
+		for (int i = 0; i < 5; i++) {
+			System.out.println(con[i].getMobileNo() + "\t" + con[i].getName() + "\t\t" + con[i].getEmailId());
+		}
 	}
-}
-	
-	public static void main(String[] args) 
-	{
-		char addmoreContact='y', moreOperations='y';
-	
-		Scanner sc=new Scanner(System.in);
-		
-		do
-		{
+
+	public static void main(String[] args) {
+		char moreOperations = 'y';
+		Scanner sc = new Scanner(System.in);
+		Contact[] con = new Contact[5];
+
+		do {
 			System.out.print("Enter Password to unlock mobile : ");
-			userPw=sc.nextInt();
-		}
-		
-		while(password!=userPw);
-		
-		
-		while(moreOperations=='y' || moreOperations=='Y')
-		{
-			System.out.println("\t"+"Press 1 to ADD CONTACT"  + "\n"
-					+"\t"+"Press 2 to EDIT CONTACT" + "\n"
-					+"\t"+"Press 3 to DELETE CONTACT" + "\n"
-					+"\t"+"Press 4 to SHOW ALL THE CONTACTS AND TOTAL OF CONTACT SAVED" + "\n"
-					+"\t"+"Press 5 to SEARCH CONTACT BASED ON NAME, MOBILE,EMAIL" + "\n");
-			
-					int input = sc.nextInt();
-					
-					switch(input) 
-					{
-						case 1:
-							addContact();
-							break;
-						
-						case 2://Search Contact base on Name, Mobile or Email
-							
-							searchContact();
-							searchContactBymobileNo();
-							
-						break;
-							
-						case 4://To display all the contact details:
-							showAllContacts();
-							break;
-						default:
-							
-							System.out.println("Choose correct option  ");
-						
-					}//Switch case loop closed	
-					System.out.println("Do you want to use further operations?..y/n : ");
-					moreOperations=sc.next().charAt(0);
-		
-		}//while loop for more operations closed 
+			userPw = sc.nextInt();
+		} while (password != userPw);
 
+		while (moreOperations == 'y' || moreOperations == 'Y') {
+			System.out.println("\t" + "Press 1 to ADD CONTACT" + "\n" + "\t" + "Press 2 to EDIT CONTACT" + "\n" + "\t"
+					+ "Press 3 to DELETE CONTACT" + "\n" + "\t"
+					+ "Press 4 to SHOW ALL THE CONTACTS AND TOTAL OF CONTACT SAVED" + "\n" + "\t"
+					+ "Press 5 to SEARCH CONTACT BASED ON NAME, MOBILE,EMAIL" + "\n");
 
-}
-}
+			int input = sc.nextInt();
+
+			switch (input) {
+			case 1:
+				addContact(con, sc);
+				break;
+
+			case 2:// Search Contact base on Name, Mobile or Email
+				searchContact(con, sc);
+				searchContactBymobileNo(con, sc);
+				break;
+			case 3:
+				break;
+			case 4:// To display all the contact details:
+				showAllContacts(con);
+				break;
+			default:
+				System.out.println("Choose correct option  ");
+			}
+			System.out.println("Do you want to use further operations?..y/n : ");
+			moreOperations = sc.next().charAt(0);
+
+		} // while loop for more operations closed
+		sc.close();
+	}
 }
